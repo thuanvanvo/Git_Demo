94759196b7a396dcd52855ca9dbdaa0a015b4957
Thuan Van Vo <votuonganvn@gmail.com>
2023-09-17 16:50:33 +0700

    Initialize repository

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s; string result ="" ;
+    cout << "Nhập chuỗi s: " ; getline(cin, s);
+    int i = 0;
+    bool isSpaced = true;
+    int firstPos, secPos;
+    
+    // Check start and end position of '#'
+    for(int i = 0; i<s.size(); i++){
+        if(s[i] == '#'){
+            firstPos = i;
+            for(int j = i+1; j<s.size(); j++){
+                if(s[j] == '#'){
+                    secPos = j;
+                    break;
+                } 
+            }
+            break;
+        }
+    }
+    
+    //Check if the string is space
+    for(int i = firstPos+1; i<secPos; i++){
+        if(s[i] != ' ') isSpaced = false;
+    }
+    
+    //Print the result
+    if(isSpaced) cout<<"Empty";
+    else cout<< s.substr(firstPos + 1, secPos - 1);
+    
+
+    return 0;
+}
